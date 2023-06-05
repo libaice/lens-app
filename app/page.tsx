5f6947ebc2c9b2fc00c3541e8caca5b5c50d342a
libaice <libaice147@gmail.com>
2023-06-06 00:00:06 +0800

    add lens API develop

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,59 @@
+/* app/page.tsx */
 'use client'
-import { useWalletLogin, useWalletLogout, useActiveProfile } from '@lens-protocol/react-web';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
-import { InjectedConnector } from 'wagmi/connectors/injected';
 
-export default function Authentication() {
-  const { execute: login, isPending: isLoginPending } = useWalletLogin();
-  const { execute: logout } = useWalletLogout();
-  const { data: wallet, loading } = useActiveProfile();
-  const { isConnected } = useAccount();
-  const { disconnectAsync } = useDisconnect();
+import { useEffect, useState } from 'react'
+import { client, exploreProfiles } from '../api'
+import Link from 'next/link'
 
-  const { connectAsync } = useConnect({
-    connector: new InjectedConnector(),
-  });
+export default function Home() {
+  /* create initial state to hold array of profiles */
+  const [profiles, setProfiles] = useState<any>([])
+  useEffect(() => {
+    fetchProfiles()
+  }, [])
+  async function fetchProfiles() {
+    try {
+      /* fetch profiles from Lens API */
+      let response = await client.query({ query: exploreProfiles })
+      /* loop over profiles, create properly formatted ipfs image links */
+      let profileData = await Promise.all(response.data.exploreProfiles.items.map(async profileInfo => {
+        let profile = { ...profileInfo }
+        let picture = profile.picture
+        if (picture && picture.original && picture.original.url) {
+          if (picture.original.url.startsWith('ipfs://')) {
+            let result = picture.original.url.substring(7, picture.original.url.length)
+            profile.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`
+          } else {
+            profile.avatarUrl = picture.original.url
+          }
+        }
+        return profile
+      }))
 
-  const onLoginClick = async () => {
-    if (isConnected) {
-      await disconnectAsync();
-    }
-    const { connector } = await connectAsync();
-    if (connector instanceof InjectedConnector) {
-      const signer = await connector.getSigner();
-      await login(signer);
+      /* update the local state with the profiles array */
+      setProfiles(profileData)
+    } catch (err) {
+      console.log({ err })
     }
   }
- 
   return (
-    <div className="flex flex-col p-12 items-start">
-      {
-        loading && <p>Loading...</p>
-      }
-      {
-        !wallet && !loading && (
-          <button
-          className='mt-2 px-6 py-1 bg-white text-black rounded'
-            disabled={isLoginPending}
-            onClick={onLoginClick}>Sign in</button>
-        )
-      }
-      {
-        wallet && !loading && (
-          <div>
-            <h3 className="text-3xl">{wallet.handle}</h3>
-            <p>{wallet.bio}</p>
-            <button onClick={logout} className='mt-2 px-6 py-1 bg-white text-black rounded'>Sign out</button>
-          </div>
-        )
-      }
+    <div className='pt-20'>
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-5xl mb-6 font-bold'>Hello Lens 🌿</h1>
+        {
+          profiles.map(profile => (
+            <div key={profile.id} className='w-2/3 shadow-md p-6 rounded-lg mb-8 flex flex-col items-center'>
+              <img className='w-48' src={profile.avatarUrl || 'https://picsum.photos/200'} />
+              <p className='text-xl text-center mt-6'>{profile.name}</p>
+              <p className='text-base text-gray-400  text-center mt-2'>{profile.bio}</p>
+              <Link href={`/profile/${profile.handle}`}>
+                <p className='cursor-pointer text-violet-600 text-lg font-medium text-center mt-2 mb-2'>{profile.handle}</p>
+              </Link>
+              <p className='text-pink-600 text-sm font-medium text-center'>{profile.stats.totalFollowers} followers</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
-  );
+  )
 }
--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,147 +1,74 @@
-// app/profile/[id]/page.tsx
+/* app/profile/[id]/page.tsx */
 'use client'
+import { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation';
-// new imports
-import {
-  useProfile, usePublications, useFollow, useWalletLogin, useWalletLogout, useActiveProfile,
-  Profile, ProfileOwnedByMe, NotFoundError
-} from '@lens-protocol/react-web';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
-import { InjectedConnector } from 'wagmi/connectors/injected';
-import { formatPicture } from '../../../utils';
+import { client, getPublications, getProfile } from '../../../api'
 
 export default function Profile() {
-  // new hooks
-  const { execute: login } = useWalletLogin();
-  const { execute: logout } = useWalletLogout();
-  const { data: wallet } = useActiveProfile();
-  const { isConnected } = useAccount();
-  const { disconnectAsync } = useDisconnect();
-  
+  /* create initial state to hold user profile and array of publications */
+  const [profile, setProfile] = useState<any>()
+  const [publications, setPublications] = useState<any>([])
+  /* using the router we can get the lens handle from the route path */
   const pathName = usePathname()
   const handle = pathName?.split('/')[2]
-
-  let { data: profile, loading } = useProfile({ handle })
-
-  const { connectAsync } = useConnect({
-    connector: new InjectedConnector(),
-  });
   
-  // new login function
-  const onLoginClick = async () => {
-    if (isConnected) {
-      await disconnectAsync();
+  useEffect(() => {
+    if (handle) {
+      fetchProfile()
     }
-    const { connector } = await connectAsync();
-    if (connector instanceof InjectedConnector) {
-      const signer = await connector.getSigner();
-      await login(signer);
+  }, [handle])
+
+  async function fetchProfile() {
+    try {
+      /* fetch the user profile using their handle */
+      const returnedProfile = await client.query({
+        query: getProfile,
+        variables: { handle }
+      })
+      const profileData = { ...returnedProfile.data.profile }
+      /* format their picture if it is not in the right format */
+      const picture = profileData.picture
+      if (picture && picture.original && picture.original.url) {
+        if (picture.original.url.startsWith('ipfs://')) {
+          let result = picture.original.url.substring(7, picture.original.url.length)
+          profileData.avatarUrl = `http://lens.infura-ipfs.io/ipfs/${result}`
+        } else {
+          profileData.avatarUrl = profileData.picture.original.url
+        }
+      }
+      setProfile(profileData)
+            /* fetch the user's publications from the Lens API and set them in the state */
+      const pubs = await client.query({
+        query: getPublications,
+        variables: {
+            id: profileData.id, limit: 50
+        }
+      })
+      setPublications(pubs.data.publications.items)
+    } catch (err) {
+      console.log('error fetching profile...', err)
     }
   }
 
-  if (loading) return <p className="p-14">Loading ...</p>
+  if (!profile) return null
 
   return (
-    <div>
-      <div className="p-14">
+    <div className='pt-20'>
+      <div className='flex flex-col justify-center items-center'>
+        <img
+          className='w-64 rounded-full'
+          src={profile.avatarUrl}
+        />
+        <p className='text-4xl mt-8 mb-8'>{profile.handle}</p>
+        <p className='text-center text-xl font-bold mt-2 mb-2 w-1/2'>{profile.bio}</p>
         {
-          !wallet && (
-            <button className="bg-white text-black px-14 py-4 rounded-full mb-4" onClick={onLoginClick}>Sign In</button>
-          )
+            publications.map(pub => (
+              <div key={pub.id} className='shadow p-10 rounded mb-8 w-2/3'>
+                <p>{pub.metadata.content}</p>
+              </div>
+            ))
         }
-        {
-          wallet && profile && (
-            <>
-            <FollowComponent
-              isConnected={isConnected}
-              profile={profile}
-              wallet={wallet}
-            />
-            <button className="ml-4 bg-white text-black px-14 py-4 rounded-full mb-4" onClick={logout}>Sign Out</button>
-            </>
-          )
-        }
-        {
-          profile && profile.picture?.__typename === 'MediaSet' && (
-            <img
-              width="200"
-              height="200"
-              alt={profile.handle}
-              className='rounded-xl'
-              src={formatPicture(profile.picture)}
-            />
-          )
-        }
-        <h1 className="text-3xl my-3">{profile?.handle}</h1>
-        <h3 className="text-xl mb-4">{profile?.bio}</h3>
-        { profile && <Publications profile={profile} /> }
       </div>
     </div>
-  )
-}
-
-// new component
-function FollowComponent({
-  wallet,
-  profile,
-  isConnected
-} : {
-  isConnected: boolean,
-  profile: Profile,
-  wallet: ProfileOwnedByMe
-}) {
-  const { execute: follow } = useFollow({ followee: profile, follower: wallet  });
-  return (
-    <>
-      {
-        isConnected && (
-          <button
-            className="bg-white text-black px-14 py-4 rounded-full"
-            onClick={follow}
-          >Follow {profile.handle}</button>
-        )
-      }
-    </>
-  )
-}
-
-function Publications({
-  profile
-}: {
-  profile: Profile
-}) {
-  let { data: publications } = usePublications({
-    profileId: profile.id,
-    limit: 20,
-  })
-  publications = publications?.map(publication => {
-    if (publication.__typename === 'Mirror') {
-      return publication.mirrorOf;
-    } else {
-      return publication;
-    }
-  });
-
-  return (
-    <>
-      {
-        publications?.map((pub: any, index: number) => (
-          <div key={index} className="py-4 bg-zinc-900 rounded mb-3 px-4">
-            <p>{pub.metadata.content}</p>
-            {
-              pub.metadata?.media[0]?.original && ['image/jpeg', 'image/png'].includes(pub.metadata?.media[0]?.original.mimeType) && (
-                <img
-                  width="400"
-                  height="400"
-                  alt={profile.handle}
-                  className='rounded-xl mt-6 mb-2'
-                  src={formatPicture(pub.metadata.media[0])}
-                />
-              )
-            }
-          </div>
-        ))
-    }
-    </>
   )
 }
